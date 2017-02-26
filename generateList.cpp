2a22fee1520ecfc0692be130ff19eb39365c8b43
Steven Mitchell <smitch15@binghamton.edu>
2017-02-26 16:24:17 -0500

    add generateList.cpp

---
+++ generateList.cpp
@@ -0,0 +1,7 @@
+// Assoxination list generator 
+// Input: how many winners do you want, and list of names (format: file?)
+// output: list of people and their targets (format: file?)
+
+
+// Assoxinator class
+
